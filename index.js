a279d6af50f4ffac270e5589317529483d7d6cd7
saipepu <saipepu.mdy257@gmail.com>
2023-04-03 02:05:58 +0700

    changes

--- index.js
+++ index.js
@@ -16,18 +16,25 @@ const io = new Server(server, {
   }
 })
 
-io.on('connection', (socket) => {
-  console.log('One Client Connected')
-  socket.emit('server', 'A Reponse from Server')
-
-  socket.on('join_room', (room) => {
-    socket.join(room);
-    console.log('A user has joined room - ', room);
+const start = () => {
+  io.on('connection', (socket) => {
+    console.log('One Client Connected')
+    socket.emit('server', 'A Reponse from Server')
+  
+    socket.on('join_room', (room) => {
+      socket.join(room);
+      console.log('A user has joined room - ', room);
+    })
+    socket.on('sendMessage', (data) => {
+      socket.to(data.room).emit('receiveMessage', data.message);
+    })
+    socket.on('disconnect', () => {
+      console.log('A client')
+      collection.watch().close();
+    })
   })
-  socket.on('sendMessage', (data) => {
-    socket.to(data.room).emit('receiveMessage', data.message);
-  })
-})
+}
+start();
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log('Server listening on PORT ' + PORT))
