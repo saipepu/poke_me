c71e3dbc33b73a7acce0f6ec65df5856ce3e584a
saipepu <saipepu.mdy257@gmail.com>
2023-04-03 01:01:08 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const http = require('http')
+const { Server } = require('socket.io')
+
+//middleware
+app.use(cors())
+
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    method: ['GET', 'POST']
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log('One Client Connected')
+  socket.emit('server', 'A Reponse from Server')
+
+  socket.on('join_room', (room) => {
+    socket.join(room);
+    console.log('A user has joined room - ', room);
+  })
+  socket.on('sendMessage', (data) => {
+    socket.to(data.room).emit('receiveMessage', data.message);
+  })
+})
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => console.log('Server listening on PORT ' + PORT))
